e2a97fae4d8371230ad82c9cdf15f7966e3b9754
Mohammad Ali Gholizadeh <50132880+AshkanWebDev@users.noreply.github.com>
2024-05-29 16:04:01 +0330

    Add Pages

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="main"></div>
+</template>
+
+<style>
+#main {
+  background-image: url("/assets/image/main.jpg");
+  background-size: 100vw 100vh;
+  background-repeat: no-repeat, repeat;
+  width: 100lvw;
+  height: 100vh;
+}
+</style>
