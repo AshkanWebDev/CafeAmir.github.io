332761cee2f93daa1e8e33e6bc2c91758c14212d
Mohammad Ali Gholizadeh <50132880+AshkanWebDev@users.noreply.github.com>
2024-05-29 16:04:29 +0330

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,69 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      title: "منوی کافه امیر",
+      htmlAttrs: {
+        dir: "rtl",
+        lang: "fa-IR",
+      },
+      meta: [
+        { charset: "utf-8" },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1, user-scalable=no",
+        },
+        { hid: "description", name: "description", content: "" },
+        { name: "format-detection", content: "telephone=no" },
+        { name: "theme-color", content: "#833920" },
+      ],
+      script: [
+        // <script src="https://myawesome-lib.js"></script>
+        { src: "/persian-date.min.js", body: true },
+        // { src: "@/assets/js/persian-date.min.js", body: true },
+      ],
+      link: [
+        // <link rel="stylesheet" href="https://myawesome-lib.css">
+        { rel: "icon", type: "image/png", href: "/favicon.png" },
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.jsdelivr.net/gh/moalgholi/fonts@main/dist/reset.min.css",
+        },
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.jsdelivr.net/gh/moalgholi/fonts@main/dist/FontAwesome/FontAwesome.min.css",
+        },
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.jsdelivr.net/gh/moalgholi/fonts@main/dist/IRANYekan/IRANYekan.min.css",
+        },
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.jsdelivr.net/gh/moalgholi/fonts@main/dist/Vazir/Vazir.min.css",
+        },
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.jsdelivr.net/gh/moalgholi/fonts@main/dist/Shabnam/Shabnam.min.css",
+        },
+        {
+          rel: "stylesheet",
+          type: "text/css",
+          href: "https://cdn.jsdelivr.net/gh/moalgholi/fonts@main/dist/Blabeloo/SHBlabeloo.min.css",
+        },
+        { rel: "manifest", href: "/manifest.json" },
+      ],
+      // please note that this is an area that is likely to change
+      style: [
+        // <style type="text/css">:root { color: red }</style>
+        // { children: ":root { color: red }", type: "text/css" },
+      ],
+    },
+  },
+  css: ["@/assets/css/style.css"],
+});
