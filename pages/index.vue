20a8cbd3f6461adb48791488ec7dfa1e1c8a99ef
Mohammad Ali Gholizadeh <50132880+AshkanWebDev@users.noreply.github.com>
2024-05-30 18:37:07 +0330

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,27 @@
 <template>
-  <div id="main"></div>
+  <div id="main">
+    <NuxtLink to="/cold">منوی سرد</NuxtLink>
+    <NuxtLink to="/hot">منوی گرم</NuxtLink>
+  </div>
 </template>
 
 <style>
 #main {
+  display: flex;
+  flex-direction: column;
   background-image: url("/assets/image/main.jpg");
   background-size: 100vw 100vh;
   background-repeat: no-repeat, repeat;
   width: 100lvw;
   height: 100vh;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+}
+a {
+  font-size: 3rem;
+  padding-bottom: 2rem;
+  color: #fff;
 }
 </style>
