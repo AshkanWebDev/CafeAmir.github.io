3098f0705401c40add53be17e92888460c205d9a
Mohammad Ali Gholizadeh <50132880+AshkanWebDev@users.noreply.github.com>
2024-05-30 18:41:46 +0330

    Create index.vue

---
+++ pages/cold/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="cold">
+    <NuxtLink to="/">بازگشت</NuxtLink>
+    <p>منوی نوشیدنی سرد</p>
+  </div>
+</template>
+
+<style>
+#cold {
+  background-image: url("/assets/image/cold.jpg");
+  background-size: 100vw 100vh;
+  background-repeat: no-repeat, repeat;
+  width: 100lvw;
+  height: 100vh;
+}
+</style>
