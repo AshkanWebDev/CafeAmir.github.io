3cce72f250eba3da8ec40e64b1634153cbac388c
Mohammad Ali Gholizadeh <50132880+AshkanWebDev@users.noreply.github.com>
2024-05-30 18:41:43 +0330

    Create index.vue

---
+++ pages/hot/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="hot">
+    <NuxtLink to="/">بازگشت</NuxtLink>
+    <p>منوی نوشیدنی گرم</p>
+  </div>
+</template>
+
+<style>
+#hot {
+  color: #fff;
+  background-image: url("/assets/image/hot.jpg");
+  background-size: 100vw 100vh;
+  background-repeat: no-repeat, repeat;
+  width: 100lvw;
+  height: 100vh;
+}
+</style>
